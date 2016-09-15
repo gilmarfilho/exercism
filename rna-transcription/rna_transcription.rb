e60d994960ee40f681d4f57a2d2dc4cd9f2ab4d9
gilmarfilho <gaxf@hotmail.com>
2016-09-15 21:04:14 +0000

    Pangram adicionado

--- rna-transcription/rna_transcription.rb
+++ rna-transcription/rna_transcription.rb
@@ -3,12 +3,12 @@ module BookKeeping
 end
 
 class Complement
-    def self.of_dna (rna)
-        
-        if rna.gsub(/[CGTA]/, 'C' => '', 'G' => '', 'T' => '', 'A' => '').length != 0
-            ""
-        else
-            rna.gsub!(/[CGTA]/, 'C' => 'G', 'G' => 'C', 'T' => 'A', 'A' => 'U')
-        end
+  def self.of_dna (rna)
+
+    if rna.gsub(/[CGTA]/, 'C' => '', 'G' => '', 'T' => '', 'A' => '').length != 0
+      ""
+    else
+      rna.gsub!(/[CGTA]/, 'C' => 'G', 'G' => 'C', 'T' => 'A', 'A' => 'U')
     end
+  end
 end
