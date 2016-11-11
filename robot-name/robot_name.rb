6b5548d0ab58aa1aa7ffe7ddaa0ba07156673c91
gilmarfilho <gaxf@hotmail.com>
2016-11-11 16:37:39 +0000

    Robot name finished

--- robot-name/robot_name.rb
+++ robot-name/robot_name.rb
@@ -5,13 +5,26 @@ end
 class Robot
   attr_accessor :name
   
+  @@used_names = []
+  
   def initialize
-    @name = ""
-    @name << (0...2).map { ('a'..'z').to_a[rand(26)] }.join.upcase
-    @name << (0...3).map { ( 0 .. 9).to_a[rand(10)] }.join
+    @name = name_generator
+  end
+  
+  def name_generator
+    name = ""
+    name << (0...2).map { ('a'..'z').to_a[rand(26)] }.join.upcase
+    name << (0...3).map { ( 0 .. 9).to_a[rand(10)] }.join
+    if @@used_names.include?(name)
+      name_generator
+    else
+      @@used_names << name
+      name
+    end
   end
   
   def reset
+    @@used_names.delete(self.name)
     self.name = Robot.new.name
   end
 end
